19a572eb5e82cfa017681f9cdf88054779e6ffb2
Robin Trakroo <45097859+alphonsorob@users.noreply.github.com>
2022-03-25 15:25:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,6 @@
 export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 //EDIT !!!
-let x = 1;
-//change the above everytime you redeploy your contracts
 
 //command for deploying your contracts on hardhat:
 // npx hardhat run --network localhost scripts/deploy.js
